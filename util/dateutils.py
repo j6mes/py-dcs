8c29bad3e9d3693a974e86c5fb878709439d53ce
James Thorne <james@jthorne.co.uk>
2017-03-03 13:00:39 +0000

    is_date won't return date for single numbers now

--- util/dateutils.py
+++ util/dateutils.py
@@ -1,10 +1,19 @@
-from dateutil.parser import parse as dparse
+from dateutil.parser import parse as dparse, parser
 
-
+_parser_p = parser()
 def is_date(string):
     try:
         dparse(string)
-        return True
+        components = 0
+        res, _ = _parser_p._parse(string)
+        if hasattr(res, "day") and res.day is not None:
+            components+=1
+        if hasattr(res, "month") and res.month is not None:
+            components+=1
+        if hasattr(res, "year") and res.year is not None:
+            components +=1
+
+        return components>=2
     except ValueError:
         return False
 
